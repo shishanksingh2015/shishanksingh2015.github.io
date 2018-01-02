9ccb33094fea62be8740d4a66e30b8301838a175
shishank <shishank.singh@moldedbits.com>
2018-01-02 09:38:00 +0530

    adding emails to database

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJ5RF_lwfibjNXFvEX5hBFCaUZJ6EjrVM",
+    authDomain: "wanderlust-9e0d8.firebaseapp.com",
+    databaseURL: "https://wanderlust-9e0d8.firebaseio.com",
+    projectId: "wanderlust-9e0d8",
+    storageBucket: "wanderlust-9e0d8.appspot.com",
+    messagingSenderId: "423812130956"
+  };
+  firebase.initializeApp(config);
